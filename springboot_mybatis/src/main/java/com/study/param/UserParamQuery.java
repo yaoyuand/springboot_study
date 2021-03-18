1130a2dd288a9980e6548d0223adec383af4e645
yaoyuan <1530858803@qq.com>
2021-03-18 17:13:47 +0800

    问题修改

--- springboot_mybatis/src/main/java/com/study/param/UserParamQuery.java
+++ springboot_mybatis/src/main/java/com/study/param/UserParamQuery.java
@@ -3,6 +3,7 @@ package com.study.param;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.study.page.BasePage;
 import com.study.entity.User;
+import lombok.Data;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +12,7 @@ import java.util.Map;
  * @Author ytj
  * @Date 2021/3/18 15:50
  */
+@Data
 public class UserParamQuery extends BasePage<User> {
     private String name;
 
